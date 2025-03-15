ca2ba7d511b60ca9671f94732983a859101c4936
Nathan Laha <nlaha@outlook.com>
2025-03-15 02:04:10 -0700

    WIP suffix tree - reference counted

--- src/suffixtree/tree.rs
+++ src/suffixtree/tree.rs
@@ -1,5 +1,7 @@
-use slab::Slab;
+use std::cell::{Ref, RefCell};
 use std::panic;
+use std::rc::Rc;
+use std::thread::current;
 
 /**
  * Indicates the start and end of an edge in the suffix tree
@@ -16,17 +18,48 @@ pub struct Edge {
 pub struct TreeNode {
     id: usize,
     string_depth: usize,
-    parent: Option<usize>,
-    children: [Option<usize>; 5],
-    edge: Edge,
+    parent: RefCell<Option<Rc<TreeNode>>>,
+    children: RefCell<[Option<Rc<TreeNode>>; 5]>,
+    edge: RefCell<Edge>,
+}
+
+impl TreeNode {
+    pub fn set_parent(&self, parent: Rc<TreeNode>) {
+        *self.parent.borrow_mut() = Some(parent);
+    }
+
+    pub fn add_child(self: &Rc<Self>, child: Rc<TreeNode>) {
+        let child_idx = match child.edge.borrow().label.chars().next() {
+            Some(c) => get_child_index(c),
+            None => {
+                panic!("Child node has no edge label");
+            }
+        };
+
+        self.children.borrow_mut()[child_idx] = Some(child.clone());
+        child.set_parent(self.clone());
+    }
 }
 
 pub struct SuffixTree {
     // nodes are stored in a flat vector
     original_string: String,
     last_id: usize,
-    nodes: Vec<Option<TreeNode>>,
     suffixes: Vec<String>,
+    root: RefCell<Rc<TreeNode>>,
+}
+
+pub fn get_child_index(c: char) -> usize {
+    match c {
+        '$' => 0,
+        'a' => 1,
+        'c' => 2,
+        'g' => 3,
+        't' => 4,
+        _ => {
+            panic!("Unsupported character '{}' in edge label", c);
+        }
+    }
 }
 
 impl SuffixTree {
@@ -39,22 +72,17 @@ impl SuffixTree {
         let mut tree = SuffixTree {
             original_string: original_string.to_string(),
             last_id: string_length,
-            nodes: vec![None; string_length * 6],
             suffixes: Vec::with_capacity(string_length),
+            root: RefCell::new(Rc::new(TreeNode {
+                id: 0,
+                string_depth: 0,
+                parent: RefCell::new(None),
+                children: RefCell::new([None, None, None, None, None]),
+                edge: RefCell::new(Edge {
+                    label: "".to_string(),
+                }),
+            })),
         };
-
-        // insert root
-        let root = TreeNode {
-            id: 0,
-            string_depth: 0,
-            parent: None,
-            children: [None, None, None, None, None],
-            edge: Edge {
-                label: "".to_string(),
-            },
-        };
-
-        tree.nodes.push(Some(root));
 
         // build a set of suffixes with '$' appended to the end
         for i in 0..string_length {
@@ -79,98 +107,61 @@ impl SuffixTree {
      * Breaks the edge at the given index and inserts a new node
      * also populates the leaf node with the given label
      */
-    pub fn break_edge(&mut self, node_idx: usize, break_idx: usize, leaf_label: &str) {
-        let node = &mut self.nodes[node_idx].clone().expect("Node is null");
+    pub fn break_edge(&mut self, node: Rc<TreeNode>, break_idx: usize, leaf_label: &str) {
         // if it doesn't have a parent, panic
-        if node.parent.is_none() {
-            panic!("Node has no parent - cannot break edge");
-        }
+        let parent = node
+            .parent
+            .borrow()
+            .clone()
+            .expect("Node has no parent - cannot break edge");
 
-        let new_internal_node = TreeNode {
-            id: 0,
-            string_depth: node.parent.unwrap().clone() + break_idx,
-            parent: node.parent.clone(),
-            children: [None, None, None, None, None],
-            edge: Edge {
-                label: node.edge.label[..break_idx].to_string(),
-            },
-        };
+        // Get the current label
+        let current_label = node.edge.borrow().label.clone();
 
         // update the current node's edge
-        node.edge.label = node.edge.label[break_idx..].to_string();
-
-        // update the current node's parent
-        node.parent = Some(new_internal_node.id);
-
-        self.add_leaf(leaf_label, new_internal_node);
-    }
-
-    /**
-     * Adds a leaf node to the internal node
-     */
-    fn add_leaf(&mut self, leaf_label: &str, internal_node: TreeNode) {
-        // create a leaf node
-        let new_leaf_node = TreeNode {
-            id: 0,
-            string_depth: internal_node.string_depth + leaf_label.len(),
-            parent: Some(internal_node.id),
-            children: [None, None, None, None, None],
-            edge: Edge {
-                label: leaf_label.to_string(),
-            },
+        *node.edge.borrow_mut() = Edge {
+            label: current_label[break_idx..].to_string(),
         };
 
-        // add the new leaf node to the internal node
-        self.add_child(&internal_node, new_leaf_node);
-    }
-
-    /**
-     * Adds a child with a given edge to the parent node
-     * at the correct child index based on the alphabetic order of the first character
-     */
-    pub fn add_child(&mut self, parent: &TreeNode, mut child: TreeNode) {
+        // create a new internal node
         let next_id: usize = self.get_next_node_id();
-        child.id = next_id;
-        match child.edge.label.chars().next() {
-            Some(c) => match c {
-                '$' => {
-                    self.nodes[parent.id + 1] = Some(child);
-                }
-                'a' => {
-                    self.nodes[parent.id + 2] = Some(child);
-                }
-                'c' => {
-                    self.nodes[parent.id + 3] = Some(child);
-                }
-                'g' => {
-                    self.nodes[parent.id + 4] = Some(child);
-                }
-                't' => {
-                    self.nodes[parent.id + 5] = Some(child);
-                }
-                _ => {
-                    panic!("Unsupported character '{}' in edge label", c);
-                }
-            },
-            None => {
-                panic!("Child node has no edge label");
-            }
-        }
+        let new_internal_node = Rc::new(TreeNode {
+            id: next_id,
+            string_depth: parent.string_depth + break_idx,
+            parent: node.parent.clone(),
+            children: RefCell::new([None, None, None, None, None]),
+            edge: RefCell::new(Edge {
+                label: node.edge.borrow().label[..break_idx].to_string(),
+            }),
+        });
+
+        // update the current node's parent
+        new_internal_node.add_child(node.clone());
+
+        // add the leaf on the new internal node
+        let leaf_node = Rc::new(TreeNode {
+            id: next_id + 1,
+            string_depth: new_internal_node.string_depth + leaf_label.len(),
+            parent: RefCell::new(Some(new_internal_node.clone())),
+            children: RefCell::new([None, None, None, None, None]),
+            edge: RefCell::new(Edge {
+                label: leaf_label.to_string(),
+            }),
+        });
+
+        new_internal_node.add_child(leaf_node.clone());
     }
 
     /**
      * Walks down the tree and inserts new leaf for the given suffix
      */
     pub fn find_path(&mut self, suffix: &str) {
-        let mut node_index = 0; // Start with the root node index
-
         self.suffixes.push(suffix.to_string());
 
+        let mut current_node = self.root.borrow().clone();
+
         loop {
-            let current_node = self.nodes[node_index]
-                .clone()
-                .expect(format!("Node is null: {}", node_index).as_str());
-            let edge_label: &str = &current_node.edge.label;
+            let edge_label: &str = &current_node.edge.borrow().label.clone();
 
             // enumerate characters in edge label
             for (i, c) in edge_label.chars().enumerate() {
@@ -179,7 +170,7 @@ impl SuffixTree {
                 // if the suffix character is not equal to the edge character
                 // break a new edge and insert a new leaf
                 if suffix_char != c {
-                    self.break_edge(node_index, i, &suffix[i..]);
+                    self.break_edge(current_node.clone(), i, &suffix[i..]);
                     return;
                 }
             }
@@ -189,26 +180,25 @@ impl SuffixTree {
             // to the next character in the suffix
             let c = suffix.as_bytes()[edge_label.len()] as char;
             println!("{}", suffix);
-            let child_offset = match c {
-                '$' => 0,
-                'a' => 1,
-                'c' => 2,
-                'g' => 3,
-                't' => 4,
-                _ => {
-                    panic!("Unsupported character '{}' in edge label", c);
-                }
-            };
 
-            let child_node: Option<usize> = current_node.children[child_offset];
+            let child_node: Option<Rc<TreeNode>> =
+                current_node.children.borrow()[get_child_index(c)].clone();
             match child_node {
                 // there's already a child node, so we need to keep moving down the tree
                 Some(n) => {
-                    node_index = n;
+                    current_node = n;
                 }
                 // there's no child node, so we need to add a new leaf
                 None => {
-                    self.add_leaf(&suffix[edge_label.len()..], current_node.clone());
+                    current_node.add_child(Rc::new(TreeNode {
+                        id: self.get_next_node_id(),
+                        string_depth: current_node.string_depth + suffix.len(),
+                        parent: RefCell::new(Some(current_node.clone())),
+                        children: RefCell::new([None, None, None, None, None]),
+                        edge: RefCell::new(Edge {
+                            label: suffix[edge_label.len()..].to_string(),
+                        }),
+                    }));
                     return;
                 }
             }
@@ -223,11 +213,12 @@ mod test {
     #[test]
     fn test_tree_simple() {
         let tree = SuffixTree::new("a");
+    }
 
-        assert!(tree.nodes.len() == 2);
-        assert!(tree.suffixes.len() == 1);
-        assert!(tree.nodes[1].clone().unwrap().edge.label == "a");
-        assert!(tree.nodes[1].clone().unwrap().string_depth == 1);
-        assert!(tree.nodes[1].clone().unwrap().parent == Some(0));
+    #[test]
+    fn test_tree_simple2() {
+        let tree = SuffixTree::new("aca");
+
+        assert_eq!(tree.suffixes.len(), 3);
     }
 }
